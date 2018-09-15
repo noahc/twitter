67ca1724a2ec78a14a1d22d035628bb3c0367e38
nclark <nclark@merchantsbonding.com>
2018-09-14 21:07:31 -0500

    renames it message in spec to describe the actual test

--- spec/cli_twitter_spec.rb
+++ spec/cli_twitter_spec.rb
@@ -1,7 +1,7 @@
 load '/Users/noahc/projects/twitter/cli_twitter.thor'
 
 describe CliTwitter do
-  it 'hello world returns hello world string' do
+  it 'fetch url tweets returns fetch url tweets' do
     VCR.use_cassette("fetch_url_tweets") do
       expect(CliTwitter.new.fetch_url_tweets.count).to eq 20
     end
